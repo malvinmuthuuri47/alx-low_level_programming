90e9da69ab28e878f404563e9056b6526c0123f4
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-10-17 18:39:07 +0000

    Solution to display digits btwn 0 and 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * Return: Always 0 (success)
+ */
+
+itn main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
+	printf('"\n");
+
+	return (0);
+}
