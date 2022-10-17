1250495834a7c3db040e60888b8617b9aac07413
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-10-17 18:34:55 +0000

    Solution for excluding some letters of alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point of program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (la != 'e' && la != 'q')
+			putchar(la);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
