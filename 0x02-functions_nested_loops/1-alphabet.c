0170f57285a08767266bcafb4f6390f00ee5bf73
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-10-19 02:08:25 +0000

    Loops task 1 solution

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ */
+
+void print_alphabet(void)
+{
+	char lca;
+
+	for (lca = 'a'; lca <= 'z'; lca++)
+		_putchar(lca);
+
+	_putchar('\n');
+}
