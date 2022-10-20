a7d5eb741afe7ad6acfcab066cfa89ef9e9c0a8f
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-10-20 02:58:59 +0000

    Task 7 to 11 solution for loops

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,13 +8,22 @@
 
 int print_last_digit(int n)
 {
-	int ld = n % 10;
+	#include "main.h"
 
-	if (ld < 0)
-		ld *= -1;
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: number
+ * Return: value of the last digit of the the integer
+ */
 
+int print_last_digit(int i)
+{
+	int l_digit = i % 10;
 
-	_putchar(ld + '0');
+	if (l_digit < 0)
+		l_digit *= -1;
 
+	_putchar(l_digit + '0');
 	return (0);
+}
 }
