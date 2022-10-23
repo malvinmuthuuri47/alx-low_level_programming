315df167eb2e4a0af0c760211b1de045c2979b04
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-10-23 20:18:46 +0000

    0x04 task 3 solution

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,16 @@
 #include "main.h"
 
 /**
- *
+ * print_numbers - prints numbers between the specified conditions
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int a = 0;
+
+	while (a < 10)
+		_putchar(a++ + '0');
+
+	_putchar('\n');
+}
