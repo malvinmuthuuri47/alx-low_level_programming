2a1ed8165a5d5b08c4c98ed13f71610aa927ac55
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-10-28 16:57:16 +0000

    Added some function prototypes to main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,6 +10,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
 int _putchar(char c);
 
 #endif
