9ecc3dc9011533c207de2afc387599bc7eb8d33c
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-11-10 09:25:58 +0000

    Updated function prototypes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,51 +1,32 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
 
 /**
- * str_concat - concatenate two strings
- * @s1: first string
- * @s2: second string
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: the source string
  *
- * Return: a pointer to a newly allocated space in memory which
- * contains the contents of s1, followed by the contents of s2
- * and null terminated. NULL on failure.
+ * Return: returns a pointer to the duplicated string.
+ * It returns NULL if insufficient memory was available
  */
-
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
-	int i, j, len1, len2, len;
-	char *result;
+	char *copy;
+	int i, len = 0;
 
-	len1 = len2 = 0;
-
-	if (s1 != NULL)
-	{
-		i = 0;
-		while (s1[i++] != '\0')
-			len1++;
-	}
-
-	if (s2 != NULL)
-	{
-		i = 0;
-		while (s2[i++] != '\0')
-			len2++;
-	}
-
-	len = len1 + len2;
-	result = (char *)malloc(sizeof(char) * (len + 1));
-	if (result == NULL)
-	{
+	if (str == NULL)
 		return (NULL);
-	}
 
-	for (i = 0; i < len1; i++)
-		result[i] = s1[i];
-	for (j = 0; j < len2; j++)
-		result[i] = s2[j];
+	while (str[len] != '\0')
+		len++;
 
-	result[len] = '\0';
+	copy = (char *)malloc((sizeof(char) * len) + 1);
+	if (copy == NULL)
+		return (NULL);
 
-	return (result);
+	for (i = 0; i < len; i++)
+		copy[i] = str[i];
+	copy[len] = '\0';
+
+	return (copy);
 }
