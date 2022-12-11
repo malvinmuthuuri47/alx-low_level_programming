26a1e9be28269b91925ca013d15317c8f3af2bdd
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-12-11 10:06:33 +0000

    Correct bugs in qn1 solution

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,16 +1,19 @@
+/*
+ * File: 0-read_textfile.c
+ * Auth: Dr Marcus.
+ */
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * read_textfile - Reads a text file and prints it to POSIx stdout
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
  * @filename: A pointer to the name of the file.
  * @letters: The number of letters the
- *		function should read and print
+ *           function should read and print.
+ *
  * Return: If the function fails or filename is NULL - 0.
- *		O/w - the actual number of bytes the
- *		function can read and print
+ *         O/w - the actual number of bytes the function can read and print.
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t o, r, w;
@@ -34,7 +37,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	free(buffer);
-	close(0);
+	close(o);
 
 	return (w);
 }
