3884325b5918ece3ea1b044a6e4f8f1b9b777c0b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2022-12-11 06:41:25 +0000

    Task one solution

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,32 +1,47 @@
 #include "main.h"
 
 /**
- * string_nconcat - concatenate two strings
- * @s1: first string
- * @s2: second string
- * @n: index
- * Return: char pointer
+ * string_nconcat - a function that concatenates two strings.
+ *
+ * @s1: first char
+ * @s2: secound char
+ * @n: unsigned int
+ *
+ * Return: If the function fails, it should return NULL
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *p;
-
-	unsigned int size1 = 0, size2 = 0, i;
+	unsigned int x, y, z;
+	char *s;
 
 	if (s1 == NULL)
-		s1 = "";
-
+	{
+		x = 0;
+	}
+	else
+	{
+		for (x = 0; s1[x]; ++x)
+		;
+	}
 	if (s2 == NULL)
-		s2 = "";
-
-	while (s1[size1] != '\0')
 	{
-		size1++;
+		y = 0;
 	}
-
-	while (s2[size2] != '\0')
+	else
 	{
-		size2++;
+		for (y = 0; s2[y]; ++y)
+		;
 	}
+	if (y > n)
+		y = n;
+	s = malloc(sizeof(char) * (x + y + 1));
+	if (s == NULL)
+		return (NULL);
+	for (z = 0; z < x; z++)
+		s[z] = s1[z];
+	for (z = 0; z < y; z++)
+		s[z + x] = s2[z];
+	s[x + y] = '\0';
+	return (s);
 }
+
