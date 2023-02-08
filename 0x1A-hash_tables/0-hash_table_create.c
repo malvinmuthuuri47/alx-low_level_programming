fbaea65197965595ab512037db05644ee2a0ed8f
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-02-08 09:55:28 +0300

    Task zero solution

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,11 @@
 #include "hash_tables.h"
-#include <stddef.h>
-#include <stdlib.h>
 
 /**
  * hash_table_create - A function that creates a hash table
  * @size: The size of the array
  *
  * Return: If an error occurs - NULL
- *         Otherwise - A pointer to the new hash table
- *
+ *         Otherwise - a pointer to the new hash table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
@@ -16,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *hash_table_created;
 	unsigned long int i;
 
-	/* Allocate space for the hash table */
+	/* allocate space for the hash table */
 	hash_table_created = malloc(sizeof(hash_table_t));
 
 	if (hash_table_created == NULL)
