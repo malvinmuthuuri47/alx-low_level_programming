55e5ff23785f3ec94522cdf2b4fe258c49253b55
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-12 18:03:35 +0300

    Task nine correction

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -41,29 +41,31 @@ int ppf(int *array, size_t low, size_t high, int value)
 {
 	size_t pos;
 
-	pos = low + (((double)(high - low) / (array[high] - array[low]))
-			* (value - array[low]));
-	if (pos >= high + low)
+	if (low <= high)
 	{
-		printf("Value checked array[%ld] is out of range\n", pos);
-		return (-1);
-	}
+		pos = low + (((double)(high - low) / (array[high] - array[low]))
+				* (value - array[low]));
+		if (pos >= high + low)
+		{
+			printf("Value checked array[%ld] is out of range\n", pos);
+			return (-1);
+		}
 
-	printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 
-	if (array[pos] == value)
-	{
-		return (pos);
+		if (array[pos] == value)
+		{
+			return (pos);
+		}
+		else if (array[pos] > value)
+		{
+			return (ppf(array, low, high - 1, value));
+		}
+		else
+		{
+			return (ppf(array, low + 1, high, value));
+		}
 	}
-	else if (array[pos] > value)
-	{
-		return (ppf(array, low, high - 1, value));
-	}
-	else
-	{
-		return (ppf(array, low + 1, high, value));
-	}
-
 	return (-1);
 }
 
