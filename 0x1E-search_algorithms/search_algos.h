ff47253f2854e97b717354f2f59d710a3d32e155
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-12 14:11:53 +0300

    Header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,15 @@
+#ifndef SEARCH_ALGOS_H
+
+#define SEARCH_ALGOS_H
+
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t low, size_t high);
+int jump_search(int *array, size_t size, int value);
+size_t min(size_t a, size_t b);
+
+#endif
+
