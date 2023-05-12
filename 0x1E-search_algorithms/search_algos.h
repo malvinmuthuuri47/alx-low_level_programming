68b0814fb9e14c4e24bf6568a1b0e8d343a9edd6
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-12 17:33:49 +0300

    Updated header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,8 @@ int binary_search(int *array, size_t size, int value);
 void print_array(int *array, size_t low, size_t high);
 int jump_search(int *array, size_t size, int value);
 size_t min(size_t a, size_t b);
+int interpolation_search(int *array, size_t size, int value);
+int ppf(int *array, size_t low, size_t high, int value);
 
 #endif
 
